abf635552dac0027e4bc1439838cd55e09be5933
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-09-30 16:19:26 +0330

    add progress bar for upload and download

--- telegramcloud/scripts/tginfo.py
+++ telegramcloud/scripts/tginfo.py
@@ -3,6 +3,8 @@ from ..utils.utils import session_retriever
 from ..utils.args import (
     tginfo_args
 )
+import json
+
 args = tginfo_args()
 
 
@@ -14,8 +16,13 @@ client = session_retriever(TelegramClient, unique_name)
 async def main():
 
     async with client:
-        messages = await client.get_messages(args.username, limit=2)
-        print("messages no: ",(messages))
+        dialogs = await client.get_dialogs()
+        first = dialogs[1]
+        print(first.title)
+        # messages = await client.get_messages(args.username, limit=20)
+        # for message in messages:
+        #     # pdf_name = message.
+        #     print(pdf_name)    
 
 def cli():
     with client:

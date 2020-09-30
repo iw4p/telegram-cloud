7088a2abc02718f89c36ce509a24e4331930e352
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-09-30 19:45:04 +0330

    add tginfo for getting number of things inside chats

--- telegramcloud/utils/args.py
+++ telegramcloud/utils/args.py
@@ -19,10 +19,6 @@ def tginfo_args():
     parser = argparse.ArgumentParser()
     # Read arguments from the command line
     parser.add_argument("--name", "-n", help = "That unique name you've set at the first", required = True)
-    parser.add_argument("--username", "-u", help = "Set target username for sending file/files to her/him", required = True)
-    # parser.add_argument("--path", "-p", help = "Path of the file/files you want to upload", required = True)
-    # parser.add_argument("--mode", "-m", help="Change mode to upload or download", required=True)
-    # parser.add_argument("--caption", "-c", help = "for upload mode: write caption/text under file, for download mode: download file by its caption or name", default = '', required = False)
-    
+    parser.add_argument("--username", "-u", help = "Set target username for getting info from conversation", required = True)    
     args = parser.parse_args()
     return args

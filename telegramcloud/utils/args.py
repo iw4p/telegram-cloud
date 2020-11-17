73db960eb21609690f56156b9b361260ab6a76c3
anightshade <avery.nightshade@gmail.com>
2020-11-18 01:48:22 +0200

    Command line option to select format for the file caption

--- telegramcloud/utils/args.py
+++ telegramcloud/utils/args.py
@@ -9,6 +9,8 @@ def tgcloud_args():
     parser.add_argument("--username", "-u", help = "Set target username for sending file/files to her/him", required = True)
     parser.add_argument("--path", "-p", help = "Path of the file/files you want to upload", required = True)
     parser.add_argument("--mode", "-m", help="Change mode to upload or download", required=True)
+    parser.add_argument("--format", default="text", dest="parse_mode", choices=["text", "markdown", "html"],
+                        help="Parse mode for file caption. Choose from 'text', 'markdown', or 'html'. Default is 'text'")
     parser.add_argument("--caption", "-c", help = "for upload mode: write caption/text under file, for download mode: download file by its caption or name", default = '', required = False)
     
     args = parser.parse_args()
